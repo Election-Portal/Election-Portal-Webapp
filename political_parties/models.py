c98f8ae33db807da5eccd9356b56babc9680b2b9
Nischal Lal Shrestha <aakrist666@gmail.com>
2018-07-04 22:43:55 +0545

    Models for political part

--- political_parties/models.py
+++ political_parties/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class PoliticalParty(models.Model):
+    name = models.CharField("Name of Party", max_length=50)
+    founded_on = models.DateField("Date of Establishment", auto_now=False, auto_now_add=False)
+    ideology = models.TextField()
+    seats_in_pradessabha = models.IntegerField()
+    seats_in_pratinidhisabha = models.IntegerField()
+    chief_of_party = models.CharField("President of Party", max_length=50)
+    about = models.TextField()
+    headquarter = models.CharField("Headquarter Location", max_length=50)
+
+    class Meta:
+        verbose_name = "Political Party"
+        verbose_name_plural = "Political Parties"
+
+    def __str__(self):
+        return self.name
