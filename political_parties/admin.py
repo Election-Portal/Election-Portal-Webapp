8468455a006845a50b10985914adeef76b5556d3
Nischal Lal Shrestha <aakrist666@gmail.com>
2018-07-04 23:03:08 +0545

    adds political parties in the admin section

--- political_parties/admin.py
+++ political_parties/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from political_parties.models import PoliticalParty
 # Register your models here.
+
+@admin.register(PoliticalParty)
+class PoliticalPartyAdmin(admin.ModelAdmin):
+    '''Admin View for PoliticalParty'''
+
+    list_display = ('name', 'chief_of_party', 'founded_on')
+    ordering = ('name',)
