2d50ece6f58fcd2c35c5a67e1c59bc3642204f54
Nischal Lal Shrestha <aakrist666@gmail.com>
2018-07-04 22:43:13 +0545

    Models for Candidates

--- candidates/admin.py
+++ candidates/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from candidates.models import Nominee
 # Register your models here.
+
+
+@admin.register(Nominee)
+class NomineeAdmin(admin.ModelAdmin):
+    '''Admin View for Nominee'''
+
+    list_display = ('full_name', 'political_affiliation', 'dob')
+    ordering = ('full_name',)
--- candidates/models.py
+++ candidates/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from political_parties.models import PoliticalParty
 # Create your models here.
+
+
+class Nominee(models.Model):
+    full_name = models.CharField("Full Name", max_length=50)
+    political_affiliation = models.ForeignKey(PoliticalParty, related_name = "nominee_political_affiliation_set", on_delete=models.CASCADE)
+    dob = models.DateField("Date of Birth", auto_now=False, auto_now_add=False)
+    academic_background = models.TextField()
+    awards = models.TextField()
+    net_worth = models.IntegerField()
+    ideology = models.TextField()
+    is_incumbent = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.full_name
+
+
