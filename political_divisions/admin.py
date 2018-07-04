14e019ad5a0f8026bf846048f93960dfd55e291c
Nischal Lal Shrestha <aakrist666@gmail.com>
2018-07-04 21:47:46 +0545

    Informs admin about the province model

--- political_divisions/admin.py
+++ political_divisions/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from political_divisions.models import Province
 
 # Register your models here.
+
+@admin.register(Province)
+class ProvinceAdmin(admin.ModelAdmin):
+    '''Admin View for Province'''
+
+    list_display = ('name', 'capital_city', 'chief_minister', 'area', 'population')
+    ordering = ('name',)
