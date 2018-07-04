b17ef9d607007805de748acb2544de82c3df4fac
Nischal Lal Shrestha <aakrist666@gmail.com>
2018-07-04 21:40:28 +0545

    Creates models for the province

--- political_divisions/models.py
+++ political_divisions/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Province(models.Model):
+    name = models.CharField("Province Name", max_length=50)
+    formation_date = models.DateField("Province was formed on", auto_now=False, auto_now_add=False)
+    capital_city = models.CharField("Capital City", max_length=50)
+    area = models.IntegerField()
+    population = models.IntegerField()
+    chief_minister = models.CharField("Chief Minister", max_length=50)
+    speaker = models.CharField(max_length=50)
+    governor = models.CharField(max_length=50)
+    # official_logo = models.ImageField(, upload_to=None, height_field=None, width_field=None, max_length=None)
+
+    def __str__(self):
+        return self.name
