e5881911215489d3d9eb15a6dd6ea15827bdacb4
Nischal Lal Shrestha <aakrist666@gmail.com>
2018-07-06 22:07:49 +0545

    Adds modelform to add province

---
+++ political_divisions/forms.py
@@ -0,0 +1,11 @@
+from django.forms import ModelForm
+from political_divisions.models import Province
+
+class ProvinceForm(forms.ModelForm):
+    """Form definition for Province."""
+
+    class Meta:
+        """Meta definition for Provinceform."""
+
+        model = Province
+        fields = '__all__'
