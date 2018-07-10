331e704011ea9597bf586f231f5e473b27c6394a
Nischal Lal Shrestha <aakrist666@gmail.com>
2018-07-10 23:00:32 +0545

    Urls to view candidate and their details

--- candidates/urls.py
+++ candidates/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from candidates.views import add_nominee
+from candidates.views import add_nominee, nominees_list, nominee_details
 
 urlpatterns = [
-    path('add-nominee/', add_nominee, name="Add Nominee"),
+    path('add-nominee/', add_nominee, name="AddNominee"),
+    path('', nominees_list, name = "AllNominees"),
+    path('details-<int:pk>/', nominee_details, name="NomineeDetail" ),
 ]
