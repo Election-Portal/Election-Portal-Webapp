38efaa0dc7fa49f5ab6b07db42805586a9bcb6ff
Nischal Lal Shrestha <aakrist666@gmail.com>
2018-07-10 23:00:58 +0545

    Creates views which will list all the candidates and their details

--- candidates/views.py
+++ candidates/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from candidates.forms import NomineeForm
+from candidates.models import Nominee
 from django.views import View
 
 # Create your views here.
@@ -21,3 +22,21 @@ def add_nominee(request):
         'form':form,
     }
     return render(request, template_name, context)
+
+def nominees_list(request):
+    all_nominees = Nominee.objects.all().order_by('full_name')
+    template_name = "candidates/list.html"
+    context = {
+        "all_nominees":all_nominees,
+    }
+    return render(request, template_name, context)
+
+def nominee_details(request, pk):
+    # Change PK with slug
+    nominee = get_object_or_404(Nominee,pk=pk)
+    template_name = "candidates/details.html"
+    context ={
+        "nominee":nominee,
+    } 
+    return render(request,template_name,context)
+    
