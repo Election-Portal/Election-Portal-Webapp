ff35cb694c43c01a927159c45011442daf87a67f
Nischal Lal Shrestha <aakrist666@gmail.com>
2018-07-12 21:19:12 +0545

    urls and views to add new political parties

--- political_parties/urls.py
+++ political_parties/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from political_parties.views import political_parties_list, political_parties_detail
+from political_parties.views import political_parties_list, political_parties_detail, add_political_party
 
 urlpatterns = [
     path('', political_parties_list, name="AllPoliticalParties"),
     path('details-<int:pk>', political_parties_detail, name="PoliticalPartyDetail"),
+    path('add-political-party/', add_political_party, name="AddPoliticalParty"),
 ]
--- political_parties/views.py
+++ political_parties/views.py
@@ -1,6 +1,38 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404, HttpResponseRedirect
 from political_parties.models import PoliticalParty
+from political_parties.forms import PoliticalPartyForm
+from django.urls import reverse
 # Create your views here.
+
+def add_political_party(request):
+    if request.method == "POST":
+        form = PoliticalPartyForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('HomePage'))
+    else:
+        form = PoliticalPartyForm()
+    
+    template_name = "political_parties/add_political_party.html"
+    context = {
+        "form":form,
+    }
+    return render(request, template_name, context)
+# def add_nominee(request):
+#     if request.method == "POST":
+#         form = NomineeForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponse("Nominee is addes successfully.")
+    
+#     else:
+#         form = NomineeForm()
+    
+#     template_name = "candidates/add_nominee.html"
+#     context = {
+#         'form':form,
+#     }
+#     return render(request, template_name, context)
 
 def political_parties_list(request):
     all_political_parties = get_list_or_404(PoliticalParty)
@@ -18,7 +50,8 @@ def political_parties_detail(request, pk):
         "political_party":political_party,
     }
     return render(request, template_name, context)
-    
+
+
 
 # def member_list(request):
 #     all_member = Member.objects.all().order_by('full_name')
