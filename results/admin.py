7dd42db14142f8d3f2adaea1662acb7c09d4b359
Nischal Lal Shrestha <aakrist666@gmail.com>
2018-07-15 21:12:24 +0545

    Models Election Results

--- results/admin.py
+++ results/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from results.models import PradeshSabhaResult, PratinidhiSabhaResult
+from candidates.models import Nominee
 # Register your models here.
+
+
+
+
+@admin.register(PratinidhiSabhaResult)
+class PratinidhiSabhaResultAdmin(admin.ModelAdmin):
+    '''Admin View for PratinidhiSabhaResult'''
+
+    list_display = ('year', 'pratinidhisabha', 'is_declared')
+
+@admin.register(PradeshSabhaResult)
+class PradeshSabhaAdmin(admin.ModelAdmin):
+    '''Admin View for PradeshSabhaResult'''
+
+    list_display = ('year', 'pradeshsabha', 'is_declared')
--- results/models.py
+++ results/models.py
@@ -1,3 +1,44 @@
 from django.db import models
-
+from electoral_constituencies.models import PratinidhiSabha, PradeshSabha
 # Create your models here.
+
+ELECTION_YEAR_CHOICES = (
+    ('2074', '2074'),
+)
+
+class Result(models.Model):
+    year = models.CharField(max_length=4, choices=ELECTION_YEAR_CHOICES)
+
+    def __str__(self):
+        return self.year
+    
+    class Meta:
+        verbose_name="Result"
+        verbose_name_plural="Results"
+
+class PratinidhiSabhaResult(Result):
+    pratinidhisabha = models.ForeignKey(PratinidhiSabha, on_delete=models.CASCADE)
+    is_declared = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "Result of {} - {}".format(self.pratinidhisabha, self.year)
+    
+    class Meta:
+        verbose_name = "Result of Pratinidhi Sabha"
+        verbose_name_plural = "Result of Pratinidhi Sabhas"
+    
+
+class PradeshSabhaResult(Result):
+    pradeshsabha = models.ForeignKey(PradeshSabha, on_delete=models.CASCADE)
+    is_declared = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "Result of {} - {}".format(self.pradeshsabha, self.year)
+    
+    class Meta:
+        verbose_name = "Result of Pradesh Sabha"
+        verbose_name_plural = "Result of Pradesh Sabhas"
+
+    
+    
+    
