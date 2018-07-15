fcb626b4c0830d94d6e1adcda3de348a568aabcc
Nischal Lal Shrestha <aakrist666@gmail.com>
2018-07-15 20:02:02 +0545

    Adds raw text for sample presentation

--- issues/views.py
+++ issues/views.py
@@ -24,3 +24,26 @@ from issues.forms import ComplainForm
 #             'form':form,
 #         }
 #         return render(request,self.template_name,context)
+
+# class HomepageView(View):
+#     form_class = ComplainForm
+#     template_name = 'homepage/index.html'
+
+#     def post(self, request, *args, **kwargs):
+#         complain_form = self.form_class(request.POST, request.FILES, request)
+#         if complain_form.is_valid():
+#             complain_form.save()
+#             return HttpResponseRedirect(reverse('HomePage'))
+#         context = {
+#             'complain_form':complain_form,
+#         }
+#         return render(request,self.template_name, context)
+    
+#     def get(self, request, *args, **kwargs):
+#         current_url = self.request.build_absolute_uri
+#         complain_form = self.form_class(initial={'url':current_url})
+#         context = {
+#             'complain_form':complain_form,
+#         }
+#         return render(request,self.template_name,context)
+
