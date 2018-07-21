18aef35a57baaad32f7e31104ba184ab36765928
Nischal Lal Shrestha <aakrist666@gmail.com>
2018-07-21 15:28:52 +0545

    Fixes filtered result not showing in result filter form

--- results/urls.py
+++ results/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from results.views import show_all_results, show_filter_sabhas, pratinidhisabha_result_details
+from political_divisions.views import show_district_options
 
 
 
@@ -7,6 +8,8 @@ from results.views import show_all_results, show_filter_sabhas, pratinidhisabha_
 
 urlpatterns = [
     path('', show_all_results, name="AllResults"),
+    path('show-district-options/', show_district_options, name="ShowDistrictOptions"),
+
     path('<str:province>/<str:district>/', show_filter_sabhas, name="ShowFilterSabhas"),
     path('pratinidhi-sabha-result-<int:pratinidhi_sabha_pk>-details/', pratinidhisabha_result_details, name="PratinidhiSabhaResultDetails"),
 ]
